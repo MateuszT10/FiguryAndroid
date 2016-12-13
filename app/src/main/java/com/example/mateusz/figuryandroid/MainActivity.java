a181570a0a20d1aeccb607b5d281fb35dbf8d6eb
Mateusz-Laptop\Mateusz <t.mateusz10@gmail.com>
2016-12-13 23:45:42 +0100

    Dodanie metod od utworzenie N figur z zakresem cech od oraz utworz do 1 oraz z tworzeniem figur z cechami od min do max

--- app/src/main/java/com/example/mateusz/figuryandroid/MainActivity.java
+++ app/src/main/java/com/example/mateusz/figuryandroid/MainActivity.java
@@ -5,6 +5,7 @@ import android.support.v7.app.AppCompatActivity;
 import android.widget.ArrayAdapter;
 import android.widget.GridView;
 
+import com.example.Figura;
 import com.example.Program;
 
 import java.util.ArrayList;
@@ -18,8 +19,25 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_lista);
         Program program = new Program(); //obiekt klasy program
-       // program.addFigura(); // dodanie figury
-       // program.addFigura(); // dodanie figury
+        utworzNFigur(5, program); // tworzenie figur
+        tablicaStringow(program.tablicaFigur); // tablica stringow do wyswietlania w gridzie
+        GridView gridview = (GridView) findViewById(R.id.gridview);
 
     }
+    private List<String> tablicaStringow(List<Figura> tempFigury){ //zwraca tablice stringow do wyswietlania w gridzie
+        List<String> tablicaStringow = new ArrayList<String>();
+        for(int i=0; i<tempFigury.size();i++){
+            tablicaStringow.add(tempFigury.get(i).getFiguraString());
+            tablicaStringow.add(tempFigury.get(i).getCechaString());
+            tablicaStringow.add(tempFigury.get(i).getPoleString());
+        }
+        return tablicaStringow;
+    }
+    private void utworzNFigur(int N,Program temp){ // tworzy N Figur z zakresem wartosci cech od 0 do 1
+        for(int i =0; i<2;i++)  temp.addFigura((float)0.0,(float)1.0);
+    }
+    private void utworzNFigur(int N,Program temp,float min,float max){ // tworzy N Figur z zakresem wartosci cech od min do max
+        for(int i =0; i<2;i++) temp.addFigura(min , max);
+    }
+
 }
