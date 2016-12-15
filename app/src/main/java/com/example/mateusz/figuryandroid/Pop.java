41ff728ecaed2f756d1479e333d98abc31a4e300
Mateusz-Laptop\Mateusz <t.mateusz10@gmail.com>
2016-12-15 16:11:26 +0100

    Wyskakuje okienko do zmiany liczby generowanych figur(ale jeszcze nie tworzymy tych figur xD)

--- app/src/main/java/com/example/mateusz/figuryandroid/Pop.java
+++ app/src/main/java/com/example/mateusz/figuryandroid/Pop.java
@@ -2,6 +2,7 @@ package com.example.mateusz.figuryandroid;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.util.DisplayMetrics;
 
 /**
  * Created by Mateusz on 2016-12-15.
@@ -9,7 +10,15 @@ import android.os.Bundle;
 public class Pop extends Activity {
     @Override
         protected void onCreate(Bundle savedInstanceState){
+        super.onCreate(savedInstanceState);
             setContentView(R.layout.popup);
+
+        DisplayMetrics dm = new DisplayMetrics();
+        getWindowManager().getDefaultDisplay().getMetrics(dm);
+
+        int width = dm.widthPixels;
+        int height = dm.heightPixels;
+        getWindow().setLayout((int)(width*.9),(int)(height*.6));
     }
 
 }
